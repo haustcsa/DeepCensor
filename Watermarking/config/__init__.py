1b96b80156cc1fefc41d21bd9513ba84eeb1a061
HAUSTCSA Laboratory <77826105+haustcsa@users.noreply.github.com>
2026-01-14 18:27:04 +0800

    Delete Watermarking directory

--- Watermarking/config/__init__.py
+++
@@ -1,6 +0,0 @@
-
-import yaml
-from easydict import EasyDict
-
-with open("E:/scpcode/WaveGuard-master14/config/train.yaml", "r") as f:
-    training_config = EasyDict(yaml.load(f, Loader=yaml.SafeLoader))
